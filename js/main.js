a4e43af987e3f8533dfb4e8952548e75d49bb6eb
jalal <subluq@gmail.com>
2016-07-26 18:28:31 +0700

    Update

--- js/main.js
+++ js/main.js
@@ -31,6 +31,24 @@ function main() {
         target: '.navbar-default',
         offset: 80
     });
+    
+    $(document).ready(function() {
+            // Show or hide the sticky footer button
+            $(window).scroll(function() {
+                if ($(this).scrollTop() > 200) {
+                    $('.go-top').fadeIn(200);
+                } else {
+                    $('.go-top').fadeOut(200);
+                }
+            });
+
+            // Animate the scroll to top
+            $('.go-top').click(function(event) {
+                event.preventDefault();
+
+                $('html, body').animate({scrollTop: 0}, 300);
+            })
+        });
 
   	/*====================================
     WOW JS
