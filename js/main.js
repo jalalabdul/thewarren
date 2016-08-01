f5ac4dc8e79678539e1aa748b3d29038522a61a4
jalal <subluq@gmail.com>
2016-08-02 03:38:23 +0700

    Update

--- js/main.js
+++ js/main.js
@@ -53,22 +53,8 @@ function main() {
                 $('html, body').animate({scrollTop: 0}, 900);
             })
         });
-    
-        var divsToHide = document.getElementsByClassName(".powered_by");
-
-    for(var i = 0; i < divsToHide.length; i++)
-    {
-    divsToHide[i].style.visibility="hidden";
-    }
 	
-function hide(obj) {
-
-    var el = document.getElementById(obj);
-
-        el.style.display = 'none';
-
-}
-
+	
   	/*====================================
     WOW JS
     ======================================*/	
--- js/mobile.js
+++ js/mobile.js
@@ -22,29 +22,10 @@ $(document).ready(function() {
     });
 
 
-$(function () {
-  // Init function
-  function skrollrInit() {
-    skrollr.init(yourOptions);
-  }
+/*====================================
+Toggle "hvr-overline-from-center" class
+======================================*/
+$(window).on('resize', function () {
+	$('.page-scroll').toggleClass('hvr-overline-from-center', $(window).width() > 768);
 
-  // If window width is large enough, initialize skrollr
-  if ($(window).width() > 767) {
-    skrollrInit();
-  }
-
-  // On resize, check window width, if too small
-  // and skrollr instance exists, destroy;
-  // Otherwise, if window is large enough
-  // and skrollr instance does not exist, initialize skrollr.
-  $(window).on('resize', function () {
-    var _skrollr = skrollr.get(); // get() returns the skrollr instance or undefined
-    var windowWidth = $(window).width();
-
-    if ( windowWidth <= 767 && _skrollr !== undefined ) {
-      _skrollr.destroy();
-    } else if ( windowWidth > 767 && _skrollr === undefined ) {
-      skrollrInit();
-    }
-  });
-});
+}).trigger('resize')
