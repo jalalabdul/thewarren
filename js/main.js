eadd3cc74a25553a9df39894972bf683ec336d44
jalal <subluq@gmail.com>
2016-08-02 20:03:33 +0700

    Update #8

--- js/main.js
+++ js/main.js
@@ -12,11 +12,22 @@
       });
 
     /*====================================
+    navbrand Logo
+    ======================================*/
+    $(window).bind('scroll', function() {
+        if ($(window).scrollTop() > 20) {
+            $('.navbar').addClass('scrolled');
+        } else {
+            $('.navbar').removeClass('scrolled');
+        }
+    });
+
+    /*====================================
     Notif
     ======================================*/
     window.onscroll = function (e) {
         var wPosition = window.pageYOffset;
-        var startRange = document.getElementById('food-section').offsetTop - 200;
+        var startRange = document.getElementById('food-section').offsetTop - 300;
         var endRange = document.getElementById('drink-section').offsetTop;
         if(wPosition > startRange && wPosition < endRange)
         {
